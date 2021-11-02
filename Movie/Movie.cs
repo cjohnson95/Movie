b753054a3206b49b1d9413c68c42ac0ad89bedff
Seth-Race <sethrace@gmail.com>
2021-11-02 14:35:13 -0400

    Initial commit

---
+++ Movie/Movie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie {
+    class Movie {
+		private static int NextId { get; set; } = 1;
+
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public string Rating { get; set; } = "PG";
+		public int Released { get; set; } // year only
+		public int RuntimeInMinutes { get; set; }
+		public string Director { get; set; }
+
+		public string Print() {
+			return $"{Id} | {Title} | {Genre} | {NextId} ";
+		}
+
+		public int GetReleased() {
+			return this.Released;
+		}
+
+		public static int CalcRuntimeInMinutes(int Hours, int Minutes) {
+			return Hours * 60 + Minutes;
+		}
+
+		public string GetRating() {
+			return this.Rating;
+		}
+
+		public void SetRating(string rating) {
+			Rating = rating;
+		}
+
+		public void SetRuntimeInMinutes(int Hours, int Minutes) {
+			this.RuntimeInMinutes = Hours * 60 + Minutes;
+		}
+
+		public Movie() { }
+		public Movie(string title, string genre, string rating,
+					int released, int runtimeInMinutes, string director) {
+			this.Id = NextId++;
+			this.Title = title;
+			this.Genre = genre;
+			this.Rating = rating;
+			this.Released = released;
+			this.RuntimeInMinutes = runtimeInMinutes;
+			this.Director = director;
+		}
+	}
+}
+    
+
---
+++ Movie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Movie {
+    class Program {
+        static void Main(string[] args) {
+
+            var shrek =
+                new Movie("Shrek", "Comedy", "PG", 2001, 90, "Jenson/Andrew");
+
+            var blackpanther =
+                new Movie("Black Panther", "Action", "PG-13", 2018, 135, "Coogler");
+
+            var thelastunicorn =
+                new Movie("The Last Unicorn", "Fantasy", "PG", 1982, 92, "Rankin/Bass");
+
+            var thepincessbride =
+                new Movie("The Princess Bride", "Fantasy", "PG", 1987, 98, "Reiner");
+
+            var abugslife =
+                new Movie("A Bug's Life", "Family", "G", 1998, 95, "Lasseter, Stanton");
+
+            Console.WriteLine(shrek.Print());
+            Console.WriteLine(blackpanther.Print());
+            Console.WriteLine(thelastunicorn.Print());
+            Console.WriteLine(thepincessbride.Print());
+            Console.WriteLine(abugslife.Print());
+            
+        }
+    }
+}
